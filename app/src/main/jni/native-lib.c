85199222d8953ec60fd19acfef422e97ca871082
khalis murfid <khalismurfid@gmail.com>
2020-12-05 21:58:14 +0700

    Finish lab 6

---
+++ app/src/main/jni/native-lib.c
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include <string.h>
+#include <jni.h>
+
+/* This is a trivial JNI example where we use a native method
+ * to return a new VM String. See the corresponding Java source
+ * file located at:
+ *
+ *   apps/samples/hello-jni/project/src/com/example/hellojni/HelloJni.java
+ */
+jstring
+Java_id_ac_ui_cs_mobileprogramming_helloworld_MainActivity_stringFromJNI( JNIEnv* env,
+                                                  jobject thiz )
+{
+#if defined(__arm__)
+  #if defined(__ARM_ARCH_7A__)
+    #if defined(__ARM_NEON__)
+      #if defined(__ARM_PCS_VFP)
+        #define ABI "armeabi-v7a/NEON (hard-float)"
+      #else
+        #define ABI "armeabi-v7a/NEON"
+      #endif
+    #else
+      #if defined(__ARM_PCS_VFP)
+        #define ABI "armeabi-v7a (hard-float)"
+      #else
+        #define ABI "armeabi-v7a"
+      #endif
+    #endif
+  #else
+   #define ABI "armeabi"
+  #endif
+#elif defined(__i386__)
+   #define ABI "x86"
+#elif defined(__x86_64__)
+   #define ABI "x86_64"
+#elif defined(__mips64)  /* mips64el-* toolchain defines __mips__ too */
+   #define ABI "mips64"
+#elif defined(__mips__)
+   #define ABI "mips"
+#elif defined(__aarch64__)
+   #define ABI "arm64-v8a"
+#else
+   #define ABI "unknown"
+#endif
+
+    return (*env)->NewStringUTF(env, "Hello from JNI anjas !  Compiled with ABI " ABI ".");
+}
+
+JNIEXPORT jstring JNICALL
+Java_id_ac_ui_cs_mobileprogramming_helloworld_MainActivity_processQuotes(JNIEnv *env, jobject thiz,
+                                                                         jstring quotes) {
+    jstring jstr;
+    char signature[] = "Original Quotes: ";
+
+    const char *cname;
+    cname = (*env)->GetStringUTFChars(env, quotes, NULL);
+
+    char s3[100] = ""; /* note that it must be large enough! */
+    strcat(s3, signature);
+    strcat(s3, cname);
+    jstr = (*env)->NewStringUTF(env, s3);
+    return jstr;
+}
